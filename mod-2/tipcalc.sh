2a7a9596bee1b708870515994149a8fa534de2b4
Chelsey Ingersoll <cjingers@asu.edu>
2020-09-26 11:09:49 -0700

    Migration from master to main. Also fixed typos in interpreter line within a few module 2 scripts

--- mod-2/tipcalc.sh
+++ mod-2/tipcalc.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/bash
 TOTAL=`echo "scale=2; ($1*0.15)+$1" | bc`
 echo "Your total bill, including 15% tip is; ${TOTAL}"
--- mod-2/tipcalc3.sh
+++ mod-2/tipcalc3.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 read -p "Please enter the cost of three items on your bill, separated by spaces: " -a BILL
 SUBTOTAL=`echo "scale=2; ${BILL[0]} + ${BILL[1]} + ${BILL[2]}" | bc`
 DUE=`echo "scale=2; ($SUBTOTAL*0.15)+$SUBTOTAL" | bc`
